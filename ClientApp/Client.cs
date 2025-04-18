d54bcf76412cab5a167669f56e2a7aef7cb050fe
Simon Hallefält <simon@hallefelt.se>
2025-04-18 13:35:48 +0200

    made simple tcp communication between server and client

---
+++ ClientApp/Client.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClientApp {
+    class Client {
+        TcpClient client;
+
+        static void Main(string[] args) {
+            new Client();
+        }
+
+        public Client() {
+            client = new TcpClient();
+            client.Connect("127.0.0.1", 5000);
+            Console.WriteLine("Client started");
+
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            
+            Console.WriteLine($"Server says: {response}");
+            client.Close();
+        }
+    }
+    
+}
---
+++ ServerApp/Server.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerAPP {
+    class Server
+    {
+        TcpListener server;
+
+        static void Main(string[] args) {
+            new Server();
+        }
+
+        public Server() {
+            server = new TcpListener(IPAddress.Any, 5000);
+            server.Start();
+            Console.WriteLine("Started the Server");
+            Run();
+        }
+
+        private void Run() {
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine("client connected");
+
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = Encoding.UTF8.GetBytes("Hello from Server");
+            stream.Write(buffer, 0, buffer.Length);
+
+            client.Close();
+        }
+    }
+}
