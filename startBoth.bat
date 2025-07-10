dd1bdfae13a9c603f0fa1bacbd25071b17f65252
Simon Hallefält <simon@hallefelt.se>
2025-07-10 17:00:25 +0200

    changed the code to build 2 clients

--- startBoth.bat
+++ startBoth.bat
@@ -1,3 +1,8 @@
 @echo off
+cd ClientApp
+dotnet build
+cd ..
+
 start cmd /k "cd ServerApp && dotnet run"
-start cmd /k "cd ClientApp && dotnet run"
+start cmd /k "cd ClientApp && bin\Debug\net9.0\ClientApp.exe"
+start cmd /k "cd ClientApp && bin\Debug\net9.0\ClientApp.exe"
